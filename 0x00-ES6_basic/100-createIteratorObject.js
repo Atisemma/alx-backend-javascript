f41c429abd52223fc6361ec1857155a8fdb55a9f
Atisemma <atisemma26@gmail.com>
2024-06-25 01:40:43 +0000

    Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,12 @@
+/*eslint-disable*/
+export default function createIteratorObject(report) {
+  return {
+    *[Symbol.iterator]() {
+      for (const department in report.allEmployees) {
+        for (const employee of report.allEmployees[department]) {
+          yield employee;
+        }
+      }
+    }
+  };
+}
