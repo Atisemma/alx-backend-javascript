29bbb69d1c7eb2454ef3ace2f80782c28e228200
Atisemma <atisemma26@gmail.com>
2024-06-26 22:53:04 +0000

    Keeping promises:Task 1

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulating an API call
+    // You can replace this with actual API logic if needed
+    setTimeout(() => {
+      resolve("API response");
+    }, 1000);
+  });
+}
+
+export default getResponseFromAPI;
